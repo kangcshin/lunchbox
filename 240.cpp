5db141a24480cb97ac0fdc52f3d8827403e051b9
Kang <kangcshin@gmail.com>
2019-11-19 11:09:33 -0500

    .

---
+++ 240.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.size()  == 0) return false;
+        
+        int m = matrix.size()-1, n = 0;
+        
+        while(m >= 0 && n < matrix[0].size()){
+            if(matrix[m][n] == target) return true;
+            
+            else if(matrix[m][n] < target) n += 1;
+            else m -= 1;
+        }
+        return false;
+    }
+};
