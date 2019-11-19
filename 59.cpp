f802e5bfb0e3ba969ae2229917d0f47635ffb1cc
Kang <kangcshin@gmail.com>
2019-11-19 12:15:02 -0500

    .

--- 59.cpp
+++ 59.cpp
@@ -1,29 +1,34 @@
-// SPIRAL MATRIX II
-
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
         std::vector<std::vector<int>> matrix(n,std::vector<int>(n,0));
-        int i = -1, j = 0, current = 1, last = n*n;
         
-        while(current <= last){
+        int i = -1, j = 0, cur_num = 1, last_num = n*n;
+        
+        while(cur_num <= last_num)
+        {
             while(i+1 < n && matrix[j][i+1] == 0){
-                matrix[j][i+1] = current++;
-                ++i;
+                matrix[j][i+1] = cur_num;
+                cur_num += 1;
+                i += 1;
             }
             while(j+1 < n && matrix[j+1][i] == 0){
-                matrix[j+1][i] = current++;
-                ++j;
+                matrix[j+1][i] = cur_num;
+                cur_num += 1;
+                j += 1;
             }
             while(i-1 >= 0 && matrix[j][i-1] == 0){
-                matrix[j][i-1] = current++;
-                --i;
+                matrix[j][i-1] = cur_num;
+                cur_num += 1;
+                i -= 1;
             }
             while(j-1 >= 0 && matrix[j-1][i] == 0){
-                matrix[j-1][i] = current++;
-                --j;
+                matrix[j-1][i] = cur_num;
+                cur_num += 1;
+                j -=1;
             }
         }
+        
         return matrix;
     }
 };
